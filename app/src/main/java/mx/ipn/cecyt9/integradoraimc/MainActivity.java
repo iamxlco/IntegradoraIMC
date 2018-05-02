2875b5cd608ba232e3c91be746869bb8adef4df6
Efrain <ska-p_ben10@hotmail.com>
2018-05-02 14:51:57 -0500

    jejejeje

--- app/src/main/java/mx/ipn/cecyt9/integradoraimc/MainActivity.java
+++ app/src/main/java/mx/ipn/cecyt9/integradoraimc/MainActivity.java
@@ -1,13 +1,24 @@
 package mx.ipn.cecyt9.integradoraimc;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ImageButton btnMenu;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnMenu = findViewById(R.id.btn);
+    }
+
+    public void goMenu () {
+        Intent menu = new Intent(MainActivity.this, menu.class);
+        startActivity(menu);
     }
 }
